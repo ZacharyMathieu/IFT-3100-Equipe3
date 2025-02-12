59aa0c10d3c4f67ab15332c926864794a05932ff
zac <zacmathieu1@gmail.com>
2025-02-12 13:22:37 -0500

    Removed commented code
    #17

--- src/model/grid.cpp
+++ src/model/grid.cpp
@@ -4,14 +4,11 @@ Grid::Grid(int w, int h)
 {
     this->w = w;
     this->h = h;
-    // this->grid = (Cell **)malloc(sizeof(Cell) * w * h);
     for (int y = 0; y < h; y++)
     {
         vector<Cell *> line = {};
         for (int x = 0; x < w; x++)
         {
-            // cout << "assigning cell!\n";
-            // this->grid[y][x] = Cell();
             line.push_back(new Cell());
         }
         grid.push_back(line);
